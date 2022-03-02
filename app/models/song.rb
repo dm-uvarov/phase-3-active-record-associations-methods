25f354978205d43bacaf3636eb35a6dda16fb3ea
Jimmy <dm.a.uvarov@gmail.com>
2022-03-02 17:21:10 -0500

    commit tree 06cf064bc974c909a6f9543420c5a4f61c3efd01

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,19 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    # binding.pry
+    self.genre.name
   end
 
   def drake_made_this
+
+    # binding.pry
+
+    # .find_or_create
+    found = Artist.all.find_by(name: "Drake")
+    
+    !!found ? self.update(artist_id: found.id) : self.update(artist_id: Artist.create(name: "Drake").id)
+
     # when this method is called it should assign the song's artist to Drake
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@ Genre.delete_all
 Artist.delete_all
 
 hotline_bling = Song.create(name: 'Hotline Bling')
+hotline_bling2 = Song.create(name: 'Hotline Bling2')
 thriller = Song.create(name: 'Thriller')
+thriller2 = Song.create(name: 'Thriller2')
 
 drake = Artist.create(name: 'Drake')
 mj = Artist.create(name: 'Michael Jackson')
@@ -15,7 +17,12 @@ hotline_bling.artist = drake
 thriller.artist = mj
 
 drake.songs << hotline_bling
+drake.songs << hotline_bling2
 mj.songs << thriller
+mj.songs << thriller2
 
 pop.songs << thriller
 rap.songs << hotline_bling
+pop.songs << thriller2
+rap.songs << hotline_bling2
+
